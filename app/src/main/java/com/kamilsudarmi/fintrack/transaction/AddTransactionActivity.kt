3907221f2db5b0f6a04c52b104e1626d8f403c9e
lenovix <ichkmlsdr@gmail.com>
2023-06-28 23:27:04 +0700

    add spinner spPaymentmethod, spCategori

--- app/src/main/java/com/kamilsudarmi/fintrack/transaction/AddTransactionActivity.kt
+++ app/src/main/java/com/kamilsudarmi/fintrack/transaction/AddTransactionActivity.kt
@@ -3,6 +3,8 @@ package com.kamilsudarmi.fintrack.transaction
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.kamilsudarmi.fintrack.MainActivity
 import com.kamilsudarmi.fintrack.R
 import com.kamilsudarmi.fintrack.databinding.ActivityAddTransactionBinding
@@ -18,6 +20,40 @@ class AddTransactionActivity : AppCompatActivity() {
         setContentView(view)
 
         button()
+        spinnerCategory()
+        spinnerPaymentMethod()
+    }
+
+    private fun spinnerPaymentMethod() {
+        val spinnerPaymentMethod: Spinner = binding.spPaymentMethod
+
+        // Ambil data metode pembayaran dari string-array payment_methods
+        val paymentMethods = resources.getStringArray(R.array.payment_methods)
+
+        // Buat adapter untuk Spinner menggunakan array tersebut
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, paymentMethods)
+
+        // Atur tampilan dropdown item
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Terapkan adapter pada Spinner
+        spinnerPaymentMethod.adapter = adapter
+    }
+
+    private fun spinnerCategory() {
+        val spinnerCategory: Spinner = binding.spCategory
+
+        // Ambil data kategori dari string-array transaction_categories
+        val categories = resources.getStringArray(R.array.transaction_categories)
+
+        // Buat adapter untuk Spinner menggunakan array tersebut
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+
+        // Atur tampilan dropdown item
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Terapkan adapter pada Spinner
+        spinnerCategory.adapter = adapter
     }
 
     private fun button() {
